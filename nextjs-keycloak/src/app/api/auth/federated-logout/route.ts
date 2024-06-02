9d071f49d963716d2a483e8f8e779017406ed9f3
jteles277 <jteles277@gmail.com>
2024-06-02 20:54:48 +0100

    Adds Base Project with keycloak auth

---
+++ nextjs-keycloak/src/app/api/auth/federated-logout/route.ts
@@ -0,0 +1,48 @@
+// src/app/api/auth/federated-logout/route.ts
+ 
+import NextResponse from "next-auth/next";
+import { JWT } from "next-auth/jwt";
+import NextAuth from "next-auth/next";
+import KeycloakProvider from "next-auth/providers/keycloak";
+ 
+function logoutParams(token: JWT): Record<string, string> {
+    return {
+        id_token_hint: token.idToken as string,
+        post_logout_redirect_uri: process.env.NEXTAUTH_URL || "",
+    };
+}
+
+function handleEmptyToken() {
+    const response = { error: "No session present" };
+    const responseHeaders = { status: 400 };
+    return NextResponse.json(response, responseHeaders);
+}
+  
+  function sendEndSessionEndpointToURL(token: JWT) {
+    const endSessionEndPoint = new URL(
+      `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/logout`
+    );
+    const params: Record<string, string> = logoutParams(token);
+    const endSessionParams = new URLSearchParams(params);
+    const response = { url: `${endSessionEndPoint.href}/?${endSessionParams}` };
+    return NextResponse.json(response);
+  }
+  
+  export async function GET(req: NextRequest) {
+    try {
+      const token = await getToken({ req })
+      if (token) {
+        return sendEndSessionEndpointToURL(token);
+      }
+      return handleEmptyToken();
+    } catch (error) {
+      console.error(error);
+      const response = {
+        error: "Unable to logout from the session",
+      };
+      const responseHeaders = {
+        status: 500,
+      };
+      return NextResponse.json(response, responseHeaders);
+    }
+  }
